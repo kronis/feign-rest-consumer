57256ec0049b001860bf954036c7feb28fc4c1ee
Markus Loupeen <mal@capra.me>
2017-05-09 20:57:38 +0200

    Updated uri

--- backend/src/main/groovy/feignrestconsumer/GreetingClient.java
+++ backend/src/main/groovy/feignrestconsumer/GreetingClient.java
@@ -8,7 +8,7 @@ import restproducer.GreetingController;
 /**
  * Created by kronis on 06/04/2017.
  */
-@FeignClient(name = "rest-producer", url = "http://producer:9090/my-dummy-api", fallback = GreetingClient.GreetingClientFallback.class)
+@FeignClient(name = "rest-producer", url = "http://producer:8080/my-dummy-api", fallback = GreetingClient.GreetingClientFallback.class)
 public interface GreetingClient extends GreetingController {
     @Component
     public static class GreetingClientFallback implements GreetingClient {
